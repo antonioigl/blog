cf3cd59267914531145c8d7b012fda5850298933
Antonio <antonio241710@gmail.com>
2020-03-26 20:14:50 +0100

    mostrando los posts desde base de datos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Post;
+
 Route::get('/', function () {
-    return view('welcome');
+    $posts = Post::latest('published_at')->get();
+    return view('welcome', compact('posts'));
+});
+
+Route::get('posts', function () {
+    return Post::all();
 });
