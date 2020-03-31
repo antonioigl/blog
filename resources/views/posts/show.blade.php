6290947a0c574ee4fff7a1ad7e28412d1d1a3642
Antonio <antonio241710@gmail.com>
2020-03-31 19:37:39 +0200

    galería de fotos

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,12 @@
 @section('meta-description', $post->excerpt)
 
 @section('content')
-    <article class="post image-w-text container">
+    <article class="post container">
+        @if($post->photos->count() === 1)
+            <figure><img src="{{ $post->photos->first()->url }}" alt="" class="img-responsive"></figure>
+        @elseif($post->photos->count() > 1)
+            @include('posts.carousel')
+        @endif
         <div class="content-post">
             <header class="container-flex space-between">
                 <div class="date">
@@ -38,6 +43,16 @@
     </article>
 @stop
 
+@push('styles')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/twitter-bootstrap.css')}}">
+@endpush
+
 @push('scripts')
     <script id="dsq-count-scr" src="//zendero.disqus.com/count.js" async></script>
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+    </script>
+    <script src="{{asset('js/twitter-bootstrap.js')}}" async></script>
 @endpush
